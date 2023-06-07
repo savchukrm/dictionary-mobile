babd79b1533e8bf020da3b8c68a0e956ceea631a
Rymma Savchuk <savchukrmm@gmail.com>
2023-06-07 18:29:33 +0300

    behavior in navigation bar was changed

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,12 +1,40 @@
-import { View, Text, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableWithoutFeedback, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { COLORS } from '../constants';
+import { COLORS, FONTS } from '../constants';
 
 const Navigation = () => {
   const navigation = useNavigation();
+  const [isMainPressed, setIsMainPressed] = useState(false);
+  const [isFolderPressed, setIsFolderPressed] = useState(false);
+  const [isProfilePressed, setIsProfilePressed] = useState(false);
+
+  const handleMainPressIn = () => {
+    setIsMainPressed(true);
+  };
+
+  const handleMainPressOut = () => {
+    setIsMainPressed(false);
+  };
+
+  const handleFolderPressIn = () => {
+    setIsFolderPressed(true);
+  };
+
+  const handleFolderPressOut = () => {
+    setIsFolderPressed(false);
+  };
+
+  const handleProfilePressIn = () => {
+    setIsProfilePressed(true);
+  };
+
+  const handleProfilePressOut = () => {
+    setIsProfilePressed(false);
+  };
 
   return (
     <View
@@ -18,24 +46,65 @@ const Navigation = () => {
         height: 90,
         paddingTop: 15,
         flexDirection: 'row',
-        paddingHorizontal: 30,
         backgroundColor: COLORS.white,
         justifyContent: 'space-between',
       }}
     >
-      <TouchableOpacity onPress={() => navigation.navigate('Main')}>
-        <Icon name="search" size={30} color={COLORS.teal} />
-      </TouchableOpacity>
+      <TouchableWithoutFeedback
+        onPressIn={handleMainPressIn}
+        onPressOut={handleMainPressOut}
+        onPress={() => navigation.navigate('Main')}
+      >
+        <View style={[{ flex: 1, alignItems: 'center' }]}>
+          <Icon
+            name="search"
+            size={isMainPressed ? 31 : 30}
+            color={isMainPressed ? COLORS.greysecond : COLORS.teal}
+          />
+          <Text style={styles.text}>Search</Text>
+        </View>
+      </TouchableWithoutFeedback>
 
-      <TouchableOpacity onPress={() => navigation.navigate('Folder')}>
-        <Icon name="folder" size={30} color={COLORS.teal} />
-      </TouchableOpacity>
+      <TouchableWithoutFeedback
+        onPressIn={handleFolderPressIn}
+        onPressOut={handleFolderPressOut}
+        onPress={() => navigation.navigate('Folder')}
+      >
+        <View style={[{ flex: 1, alignItems: 'center' }]}>
+          <Icon
+            name="folder"
+            size={isFolderPressed ? 31 : 30}
+            color={isFolderPressed ? COLORS.greysecond : COLORS.teal}
+          />
+          <Text style={styles.text}>Sets</Text>
+        </View>
+      </TouchableWithoutFeedback>
 
-      <TouchableOpacity onPress={() => navigation.navigate('Profile')}>
-        <Icon name="person" size={30} color={COLORS.teal} />
-      </TouchableOpacity>
+      <TouchableWithoutFeedback
+        onPressIn={handleProfilePressIn}
+        onPressOut={handleProfilePressOut}
+        onPress={() => navigation.navigate('Profile')}
+      >
+        <View style={[{ flex: 1, alignItems: 'center' }]}>
+          <Icon
+            name="person"
+            style={styles.icon}
+            size={isProfilePressed ? 31 : 30}
+            color={isProfilePressed ? COLORS.greysecond : COLORS.teal}
+          />
+          <Text style={styles.text}>Profile</Text>
+        </View>
+      </TouchableWithoutFeedback>
     </View>
   );
 };
 
 export default Navigation;
+
+const styles = StyleSheet.create({
+  text: {
+    color: COLORS.teal,
+    fontFamily: FONTS.regular,
+    fontSize: 10,
+  },
+});
